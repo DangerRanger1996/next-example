5c00ce51b6c90b962dace54dfddc6551d0857dbe
Julian <julian@praxishaldimann.ch>
2018-07-08 21:04:45 +0200

    Rewrite navigation component and add slider component

--- components/Navigation/navigation.tsx
+++ components/Navigation/navigation.tsx
@@ -2,23 +2,27 @@ import Link from 'next/link'
 import styles from './style.scss'
 import React from 'react';
 
-export default () =>
-    <div id="nav" className={styles.Navigation}>
-        <ul className={styles.List}>
-            <li className={styles.Element}>
-                <Link>
-                    <a href="/">
-                        <i class="material-icons">home</i>
-                    </a>
-                </Link>
-            </li>
-            <li className={styles.Element}>
-                <Link>
-                    <a href="/about">
-                        <i class="material-icons">info</i>
-                    </a>
-                </Link>
-            </li>
-        </ul>
+class Navigation extends React.Component {
+    render() {
+        return <div id="nav" className={styles.Navigation}>
+            <ul className={styles.List}>
+                <li className={styles.Element}>
+                    <Link>
+                        <a href="/">
+                            <i class="material-icons">home</i>
+                        </a>
+                    </Link>
+                </li>
+                <li className={styles.Element}>
+                    <Link>
+                        <a href="/about">
+                            <i class="material-icons">info</i>
+                        </a>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    }
+}
 
-    </div>
+export default Navigation;
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,9 +1,11 @@
 import css from './style.scss'
+import Slider from '../../components/Slider/slider'
 
 const Component = () => {
   return (
     <div>
       <h1 className={css.Title}>About Page</h1>
+        <Slider />
     </div>
   )
 };
