6f0c213c02dc95c9d3f4d993617d14e0ef4bacdd
Julian <julian@praxishaldimann.ch>
2018-07-08 12:51:39 +0200

    Use next link in index page

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,9 +1,11 @@
-import css from "./style.scss"
+import css from './style.scss';
+import Link from 'next/link';
 
 const Component = () => {
   return (
     <div className={css.example}>
-      <h1>Hallo</h1>
+      <h1>Index Page</h1>
+        <Link href="/about">Link to about</Link>
     </div>
   )
 }
